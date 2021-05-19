e2010541b37894c26a3e758729d1dfa3b5bbd453
Lara-Shaheen <larasha2001@gmail.com>
2021-05-19 11:37:48 +0300

    OrderLastChanges

--- routes/orders/orders.js
+++ routes/orders/orders.js
@@ -8,7 +8,7 @@ const isAuth=require('../../middleware/auth');
 const isCustomer=require('../../middleware/isCustomer');
 const isAdmin=require('../../middleware/isAdmin');
  router.post('/',[isCustomer], controller.orders.CreateOrder);
- router.get('/getAllOrdersAdmin', [isAdmin],controller.orders.getAllOrdersAdmin);
+ router.get('/list', [isAdmin],controller.orders.getAllOrdersAdmin);
  router.get('/', [isAuth],controller.orders.getOrders);
  router.get('/:id', [isAuth],controller.orders.GetorderByID);
 router.put('/:id',[isAuth],controller.orders.updateOrder);
